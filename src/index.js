02b951cf083094310696ed268ee6730ddf294a72
Nikolai <nikolai.ivanov3110@gmail.com>
2023-08-10 10:03:36 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = [];
+  const matchingBrackets = {};
+
+  for (const [open, close] of bracketsConfig) {
+      openingBrackets.push(open);
+      closingBrackets.push(close);
+      matchingBrackets[close] = open;
+  }
+
+  for (const char of str) {
+      if (openingBrackets.includes(char)) {
+          if (char === matchingBrackets[char] && stack.length > 0 && stack[stack.length - 1] === char) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else if (closingBrackets.includes(char)) {
+          if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+              return false;
+          }
+      } else {
+          return false;
+      }
+  }
+  return stack.length === 0;
+};
